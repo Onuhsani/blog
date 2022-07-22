66b87ae684de9d498a746a41fb185067524fe947
Onuhsani <talk2onuh@gmail.com>
2022-07-22 17:40:46 +0100

    second commit

--- database/migrations/2022_07_22_160607_create_categories_table.php
+++ database/migrations/2022_07_22_160607_create_categories_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('meta_title');
+            $table->string('slug');
+            $table->string('content');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
